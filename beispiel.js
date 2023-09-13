c0ffce37e27b44719adede1f7cec041f0ec5e022
shanthosh04 <133089176+shanthosh04@users.noreply.github.com>
2023-09-13 22:26:52 +0200

    Add files via upload

---
+++ beispiel.js
@@ -0,0 +1,4 @@
+test('1 + 1 ist gleich 2', () => {
+    expect(1 + 1).toBe(2);
+  });
+  
---
+++ formatting.js
@@ -0,0 +1,20 @@
+function getLastElement(arr) {
+    if (arr.length === 0) {
+        return undefined;
+    } else {
+        return arr[arr.length -1];
+    }
+}
+
+function isPalindrome(wort) {
+    wort= wort.toLowerCase().replace(/ /g, '');
+
+}
+
+function capitalizeWords {
+
+}
+
+function lengthOfLongestWord {
+
+}
